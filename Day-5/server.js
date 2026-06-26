cd4a9be581eb0f64d50dc6008b80ddf311a27e69
Harsh Sharma <mailittoharsh@gmail.com>
2026-06-26 14:59:04 +0530

    URI Fixed

--- Day-5/server.js
+++ Day-5/server.js
@@ -2,7 +2,7 @@ const app = require("./src/app")
 const mongoose = require("mongoose")
 
 function connectToDb(){
-    mongoose.connect("mongodb+srv://mailittoharsh_db_user:AOIHttwxrLWdeFlG@cluster0.jopknbt.mongodb.net/day-5")
+    mongoose.connect("Your Database URI goes here...")
     .then(() => {
         console.log("Connected to Database!")
     })
