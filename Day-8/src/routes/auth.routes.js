703efbeb8afabc8c5c20330d4928b6e61300ee3b
Harsh Sharma <mailittoharsh@gmail.com>
2026-07-12 22:56:35 +0530

    JWT Implementation

---
+++ Day-8/src/routes/auth.routes.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const authRouter = express.Router()
+
+authRouter.post("/register", async(req, res) => {
+    const {username, email, password} = req.body
+
+    const isUserAlreadyExists = await userModel.findOne({ email })
+
+    if(isUserAlreadyExists){
+        return res.status(400).json({
+            message: "Email already exists!"
+        })
+    }
+
+    const user = await userModel.create({
+        username, email, password
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+        email: user.email
+    }, process.env.JWT_SECRET)
+
+    res.cookie("jwt_token", token)
+
+    res.status(201).json({
+        message: "User Registered!",
+        user,
+        token
+
+    })
+})
+
+module.exports = authRouter
